400e2380f3d632dedfb07508e260b88f8478c7ea
PAMELA ABREU <abeupamm@gmail.com>
2022-03-07 16:04:10 -0300

    correção do ecercicio 5

--- fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-1-javascript-primeiros-passos/script.js
@@ -1,19 +1,19 @@
 const a = 10;
 const b = 5;
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 
 
 const valor1 = 30;
 const valor2 = 6;
 
-if (valor1>valor2){
+if (valor1 > valor2) {
     console.log(valor1);
-} else{
+} else {
     console.log(valor2)
 }
 
@@ -21,33 +21,36 @@ const valorA = 100;
 const valorB = 400;
 const valorC = 500;
 
-if (valorC<valorA>valorB){
+if (valorC < valorA > valorB) {
     console.log(valorA);
-} if (valorA<valorB>valorC){
+} if (valorA < valorB > valorC) {
     console.log(valorB);
-} else{
+} else {
     console.log(valorC);
 }
 
 const teste = -3;
 
-if (teste > 0){
+if (teste > 0) {
     console.log("POSITIVE");
-} if (teste < 0){
+} if (teste < 0) {
     console.log("NEGATIVE");
 } else {
     console.log("ZERO");
 }
 
-const angulo1 = 50;
-const angulo2 = 100;
-const angulo3 = -5;
+const angulo1 = 360;
+const angulo2 = -90;
+const angulo3 = -90;
 const soma = angulo1 + angulo2 + angulo3;
 const resultado = "Erro";
-if (soma == 180){
-    console.log("True");
-} if (soma >= 181){
-    console.log("False");
-} else {
+
+if (angulo1 >= 0 && angulo2 >= 0 && angulo3 >= 0) {
+    if (soma == 180) {
+        console.log("True");
+    } else (soma >= 181);
+        console.log("False");
+    }
+else {
     console.log(resultado);
 }
