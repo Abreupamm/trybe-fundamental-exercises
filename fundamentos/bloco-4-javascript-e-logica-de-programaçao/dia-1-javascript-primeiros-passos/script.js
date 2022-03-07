b8afb33fc80b8dff925cf8668837af4e1656f6bd
PAMELA ABREU <abeupamm@gmail.com>
2022-03-07 15:08:35 -0300

    ecercicio 3 feito

--- fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-1-javascript-primeiros-passos/script.js
@@ -1,11 +1,11 @@
-//const a = 10;
-//const b = 5;
+const a = 10;
+const b = 5;
 
-//console.log(a+b);
-//console.log(a-b);
-//console.log(a*b);
-//console.log(a/b);
-//console.log(a%b);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
 
 
 const valor1 = 30;
@@ -15,4 +15,16 @@ if (valor1>valor2){
     console.log(valor1);
 } else{
     console.log(valor2)
+}
+
+const valorA = 100;
+const valorB = 400;
+const valorC = 500;
+
+if (valorC<valorA>valorB){
+    console.log(valorA);
+} if (valorA<valorB>valorC){
+    console.log(valorB);
+} else{
+    console.log(valorC);
 }
