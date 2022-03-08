e804de5af27fe2ec62af9bb0f8da13a26b9fb3b9
PAMELA ABREU <abeupamm@gmail.com>
2022-03-08 16:17:17 -0300

    exercicio 2 e 3

--- fundamentos/bloco-4-javascript-e-logica-de-programaçao/exercicio-proposto-array-e-loop-for/script.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/exercicio-proposto-array-e-loop-for/script.js
@@ -1,4 +1,15 @@
+// exercício 1
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+
+// exercício 2
+
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+//exercíco 3
+menu.push("Contato");
+console.log(menu);
