aaae16291ebbb66f406ee853231933919207ee71
PAMELA ABREU <abeupamm@gmail.com>
2022-03-08 19:16:11 -0300

    exercicio 2

--- fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-2-array-e-loop-for/script.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-2-array-e-loop-for/script.js
@@ -2,6 +2,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercício 1
 
-for(let i = 0; i < numbers.length; i += 1){
+for (let i = 0; i < numbers.length; i += 1) {
     console.log(numbers[i]);
 }
+
+// exercício 2
+let soma = 0;
+for (let s = 0; s < numbers.length; s += 1) {
+    soma += numbers[s];
+}
+console.log(soma);
