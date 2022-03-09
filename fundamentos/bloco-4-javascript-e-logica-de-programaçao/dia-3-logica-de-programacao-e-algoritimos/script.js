053a8849775a758728c2a0640463091987393bd8
PAMELA ABREU <abeupamm@gmail.com>
2022-03-09 16:16:29 -0300

    exercicio 1

---
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-3-logica-de-programacao-e-algoritimos/script.js
@@ -0,0 +1,14 @@
+// exercício 1
+/* algoritimo:
+1- criar uma varável com valor 1
+2- criar um loop que percorra todos os números de 1 a 10
+3- multiplicar o resultado com a variável do item 1
+4- usar console log para imprimir resultado */
+
+let multiplica = 1;
+for (let i = 2; i <= 10; i += 1){
+multiplica *= i;
+}
+console.log(multiplica)
+
+
