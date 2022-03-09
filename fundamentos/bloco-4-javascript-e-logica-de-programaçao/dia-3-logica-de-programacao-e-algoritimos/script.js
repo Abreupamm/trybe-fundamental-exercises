aa5502bc184af9dcb4787fa7d7b269a4cb9c416e
PAMELA ABREU <abeupamm@gmail.com>
2022-03-09 18:57:10 -0300

    exercicio 2

--- fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-3-logica-de-programacao-e-algoritimos/script.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-3-logica-de-programacao-e-algoritimos/script.js
@@ -11,4 +11,17 @@ multiplica *= i;
 }
 console.log(multiplica)
 
+//exercício 2
+/* 1- criar uma variável que recebe a palavra
+2- criar uma variável para receber o palavra invertida
+3- criar um loop para percorrer a palavra
+4- adicionar leta por letra no inicío da variável criada no item 2
+*/
 
+
+let word = 'Pamela';
+let wordInvertido = "";
+for(let index = word.length -1; index >= 0; index -= 1){
+wordInvertido = wordInvertido.concat(word[index]);
+}
+console.log(wordInvertido);
