9e312a9e228cb75733da009b2ad8638433c05a0c
PAMELA ABREU <abeupamm@gmail.com>
2022-03-10 20:17:55 -0300

    exercicios 4.4

---
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-4-objetos-e-funções/script.js
@@ -0,0 +1,25 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+// //   1 - Imprima no console uma mensagem de 
+// boas-vindas para a personagem acima, incluindo 
+// seu nome. Valor esperado no console:
+
+function boasVindas(){
+    console.log("Boas Vindas, " + info.personagem + " !")
+}
+boasVindas()
+
+// 2 - Insira no objeto uma nova propriedade com o 
+// nome de chave 'recorrente' e o valor 'Sim' e, em
+//  seguida, imprima o objeto no console. 
+//  Valor esperado no console:
+
+function valorEsperado(recorrente){
+    Object.defineProperty(info, recorrente, "sim");
+    return info;
+}
+valorEsperado()
