212f4557513428ba496041cb02ddedf0d2d64dc2
PAMELA ABREU <abeupamm@gmail.com>
2022-03-11 09:47:33 -0300

    exercicio 7

--- fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-4-objetos-e-funções/script.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-4-objetos-e-funções/script.js
@@ -70,3 +70,17 @@ let info2 = {
       };
 
     console.log('O livro favorito de ' + leitor.nome + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+
+    // 7 - Adicione um novo livro favorito na chave livrosFavoritos , 
+    // que é um array . Atribua a esta chave um objeto contendo as 
+    // seguintes informações:
+
+
+novoLivro = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  };
+
+    leitor.livrosFavoritos.push(novoLivro);
+    console.log(leitor);
