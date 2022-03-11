7682a4130133f4374bc2e98f3a0ee063fd8110d6
PAMELA ABREU <abeupamm@gmail.com>
2022-03-11 11:06:00 -0300

    exercicio 3 sobre funçao

--- fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-4-objetos-e-funções/funcao.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-4-objetos-e-funções/funcao.js
@@ -30,3 +30,37 @@ function maiorIndice(array){
 }
 
 maiorIndice([2, 3, 6, 7, 10, 1]);
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+// Valor esperado no retorno da função: 6 .
+
+function menorIndice(array1){
+    let menor = Math.min(...array1);
+    let indice = array1.indexOf(menor,0);
+    console.log(indice);
+}
+menorIndice([2, 4, 6, 7, 10, 0, -3]);
+
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
+// Valor esperado no retorno da função: Fernanda .
+
+
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
+// Valor esperado no retorno da função: 2 .
+
+// 6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+// Valor de teste: N = 5 .
+// Valor esperado no retorno da função: 1+2+3+4+5 = 15 .
+
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+// Valor de teste: 'trybe' e 'be'
+// Valor esperado no retorno da função: true
+// verificaFimPalavra('trybe', 'be') ;
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan') ;
+// Retorno esperado: false
