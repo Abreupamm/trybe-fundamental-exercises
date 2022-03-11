2db7ba48d5423c6784793ba381b0cbf539d256c6
PAMELA ABREU <abeupamm@gmail.com>
2022-03-10 22:44:54 -0300

    exercicios 5 incompleto

--- fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-4-objetos-e-funções/script.js
+++ fundamentos/bloco-4-javascript-e-logica-de-programaçao/dia-4-objetos-e-funções/script.js
@@ -2,15 +2,14 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
 
 // //   1 - Imprima no console uma mensagem de 
 // boas-vindas para a personagem acima, incluindo 
 // seu nome. 
-function boasVindas(){
-    console.log("Boas Vindas, " + info.personagem + " !")
-}
-boasVindas()
+    console.log("Boas Vindas, " + info.personagem + "!")
+
+
 
 // 2 - Insira no objeto uma nova propriedade com o 
 // nome de chave 'recorrente' e o valor 'Sim' e, em
@@ -20,12 +19,33 @@ info['recorrente'] = 'sim';
 console.log(info);
 
 // 3 - Faça um for/in que mostre todas as chaves do objeto.
-for(dadosPerson in info){
+for (dadosPerson in info) {
     console.log(dadosPerson);
 }
 
 // 4 - Faça um novo for/in , 
 // mas agora mostre todos os valores das chaves do objeto. 
-for(valor in info){
+for (valor in info) {
     console.log(info[valor]);
 }
+// 5 - Agora, defina um segundo objeto com a mesma estrutura
+//  (as mesmas chaves) do primeiro e os seguintes valores: 
+//  'Tio Patinhas', 'Christmas on Bear Mountain, Dell's 
+//  Four Color Comics #178', 'O último MacPatinhas', 'Sim'. 
+//  Então, imprima os valores de cada objeto juntos de acordo 
+//  com cada uma das chaves
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+    for (valores in info) {
+    }
+    for(valores1 in info2){
+    }
+    console.log(info[valores]  + ' e ' + info2[valores1]);
+   
+
