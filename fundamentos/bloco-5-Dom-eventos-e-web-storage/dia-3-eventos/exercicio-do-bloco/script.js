46eb26cf9dbef70283f5e733381a1a692195c950
pamela <abreupamm@gmail.com>
2022-03-19 16:56:40 -0300

    NOME NO BOTÃO

--- fundamentos/bloco-5-Dom-eventos-e-web-storage/dia-3-eventos/exercicio-do-bloco/script.js
+++ fundamentos/bloco-5-Dom-eventos-e-web-storage/dia-3-eventos/exercicio-do-bloco/script.js
@@ -56,11 +56,18 @@ criaDiaMes();
 // Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
 // Adicione a este botão a ID "btn-holiday" .
 // Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
-let feriados ='';
+let feriados ='FERIADOS';
 function feriadosMes(s){
   let div = document.querySelector('.buttons-container');
 let butttom = document.createElement('button');
+butttom.innerText = s
 butttom.id = 'btn-holiday';
 div.appendChild(butttom);
 }
 feriadosMes(feriados);
+
+// Exercício 3:
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que 
+// possuem a classe "holiday" .
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração 
+// inicial com a cor "rgb(238,238,238)" .
