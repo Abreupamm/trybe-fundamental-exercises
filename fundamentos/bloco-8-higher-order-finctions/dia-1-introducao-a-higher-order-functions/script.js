d755560a7a59551564e3e27a94d3881e3e755280
pamela <abreupamm@gmail.com>
2022-04-07 20:23:30 -0300

    exercicio 3

--- fundamentos/bloco-8-higher-order-finctions/dia-1-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-finctions/dia-1-introducao-a-higher-order-functions/script.js
@@ -41,3 +41,35 @@ const aposta = (numero) => {
   return numero;
 };
 console.log(resultadoSorteio(aposta(5)));
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito),
+// o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma
+// função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante.
+// Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A")
+// não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const pontos = (respostaCorreta, respostaEstudante) => {
+  if (respostaCorreta === respostaEstudante) {
+    return 1;
+  }
+  if (respostaEstudante === 'N.A') {
+    return 0;
+  }
+
+  return -0.5;
+};
+
+const correcaoGabarito = (gabarito, resposta, callback) => {
+  let pontuacao = 0;
+  for (let i = 0; i < gabarito.length; i += 1) {
+    const correcao = callback(gabarito[i], resposta[i]);
+    pontuacao += correcao;
+  }
+  return `Nota final: ${pontuacao} pontos`;
+};
+
+console.log(correcaoGabarito(RIGHT_ANSWERS, STUDENT_ANSWERS, pontos));
