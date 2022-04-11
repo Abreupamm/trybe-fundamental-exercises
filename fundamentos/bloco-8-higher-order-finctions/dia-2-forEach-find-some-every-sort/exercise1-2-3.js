03d1c1fceda696bcb0cbd08f63e6c974b0b07c1b
pamela <abreupamm@gmail.com>
2022-04-11 20:57:28 -0300

    exercicio 3

--- fundamentos/bloco-8-higher-order-finctions/dia-2-forEach-find-some-every-sort/exercise1-2-3.js
+++ fundamentos/bloco-8-higher-order-finctions/dia-2-forEach-find-some-every-sort/exercise1-2-3.js
@@ -82,18 +82,16 @@ function smallerName() {
 
 
 // 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
-const expectedResult = {
-  author: {
-    birthYear: 1948,
-    name: 'George R. R. Martin',
-  },
-  genre: 'Fantasia',
-  id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  releaseYear: 1991,
-};
+// const expectedResult = {
+//   author: {
+//     birthYear: 1948,
+//     name: 'George R. R. Martin',
+//   },
+//   genre: 'Fantasia',
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   releaseYear: 1991,
+// };
 
-function getNamedBook() {
-  // escreva seu código aqui
-}
-
+const getNamedBook = books.find((elemento) => elemento.name.length === 26)
+console.log(getNamedBook);
