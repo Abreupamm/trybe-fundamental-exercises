bd5d45e708b40130bc532367784390c28576f470
pamela <abreupamm@gmail.com>
2022-04-13 19:20:12 -0300

    exercicio 2 de fixaçao - spread

--- fundamentos/bloco-8-higher-order-finctions/dia-5/fixacao-obj-destructuring.js
+++ fundamentos/bloco-8-higher-order-finctions/dia-5/fixacao-obj-destructuring.js
@@ -26,3 +26,6 @@ console.log(dados(user, jobInfos));
 //  Para isso, utilize a desestruturação de objetos em conjunto com template literals .
 // Exemplo 
 // "Hi, my name is Maria, I'm 21 years old and I'm Brazilian. I work as a Software engineer and my squad is RLL-Rocket Landing Logic"
+
+const {name, age, nationality, profession, squadInitials, squad} = dados(user, jobInfos)
+console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
